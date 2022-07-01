3d45ef1bbcb2394802d67008f9eae0617395c261
NGW <tnkngw29@gmail.com>
2022-07-01 11:18:47 +0900

    refactor lint errors.

--- src/response_times.py
+++ src/response_times.py
@@ -3,4 +3,7 @@ class ResponseTimes(object):
         pass
 
     def find_failure(self) -> list[dict[str, str]]:
+        """
+        故障状態のサーバーアドレスと、そのサーバーの故障期間を辞書に返却する。
+        """
         return [{"address": "10.20.30.1/16", "period": "20221019133324-20221019133325"}]
--- tests/test_find_failure.py
+++ tests/test_find_failure.py
@@ -1,10 +1,20 @@
 import pytest
 from response_times import ResponseTimes
 
+
 @pytest.fixture
-def response_time_instance(request):
+def response_time_instance():
+    """
+    テスト対象のクラスのインスタンスを生成して返却
+    正常系のデータを入れたtest.csvを入力に使用する。
+    """
     return ResponseTimes("test.csv")
 
+
 def test_find_failure(response_time_instance):
-    expect = [{"address": "10.20.30.1/16", "period": "20221019133324-20221019133325"}]
+    """
+    find_failureメソッドの正常系のテスト
+    """
+    expect = [{"address": "10.20.30.1/16",
+               "period": "20221019133324-20221019133325"}]
     assert response_time_instance.find_failure() == expect
